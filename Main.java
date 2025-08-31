537660eef554a6ec14c09fa11c7b8d507f3d8d3a
ankmahaj2 <ankmahaj2@publicisgroupe.net>
2025-08-31 09:29:57 +0530

    add subtraction method and example usage in main

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
- class Main {
+class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        
+        // Example subtraction
+        int result = subtract(10, 5);
+        System.out.println("Subtraction result: " + result);
+    }
+
+    // Method to perform subtraction
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
